05bb1e4152211e4e25dbd701a039f35c42d822e6
Nico Madysa <nico.madysa@cern.ch>
2022-08-16 03:27:18 +0200

    Add class IncorporatorGroup.

--- src/cernml/lsa_utils/_incorporator.py
+++ src/cernml/lsa_utils/_incorporator.py
@@ -38,25 +38,16 @@ class Incorporator:
     """
 
     def __init__(
-        self, parameter: str, *, context: str = None, user: str = None
+        self,
+        parameter: str,
+        *,
+        context: t.Optional[str] = None,
+        user: t.Optional[str] = None,
     ) -> None:
         self._parameter = _services.parameter.findParameterByName(parameter)
         if not self._parameter:
             raise NotFound(parameter)
-        self._cycle: lsa_settings.StandAloneContext
-        # pylint: disable = no-else-raise
-        if context and user:
-            raise TypeError("conflicting arguments: `context` and `user`")
-        elif context:
-            self._cycle = _services.context.findStandAloneCycle(context)
-            if not self._cycle:
-                raise NotFound(context)
-        elif user:
-            self._cycle = _services.context.findStandAloneContextByUser(user)
-            if not self._cycle:
-                raise NotFound(user)
-        else:
-            raise TypeError("missing keyword-only argument: 'context'")
+        self._cycle = _find_cycle(context=context, user=user)
 
     @property
     def parameter(self) -> str:
@@ -135,7 +126,12 @@ class Incorporator:
         # overloads.
         if isinstance(value, np.floating):
             value = float(value)
-        setting = self._build_incorporation_setting(cycle_time, value)
+        setting = _build_incorporation_setting(
+            parameter=self._parameter,
+            context=self._cycle,
+            cycle_time=cycle_time,
+            value=value,
+        )
         _services.trim.incorporate(
             lsa_settings.IncorporationRequest.builder()
             .setContext(self._cycle)
@@ -147,26 +143,223 @@ class Incorporator:
             .build()
         )
 
-    def _build_incorporation_setting(
-        self, cycle_time: float, value: float
-    ) -> lsa_settings.IncorporationSetting:
-        beam_process = self._get_beam_process_at(cycle_time)
-        beam_process_time = cycle_time - beam_process.getStartTime()
-        setting = lsa_spi.ScalarSetting(acc_value.Type.DOUBLE)
-        setting.setBeamProcess(beam_process)
-        setting.setParameter(self._parameter)
-        setting.setCorrectionValue(acc_value.ValueFactory.createScalar(value))
-        return lsa_settings.IncorporationSetting(setting, beam_process_time)
+    @classmethod
+    def _from_raw(
+        cls, parameter: lsa_settings.Parameter, context: lsa_settings.StandAloneContext
+    ) -> Incorporator:
+        result = super().__new__(cls)
+        result._parameter = parameter
+        result._cycle = context
+        return result
 
-    def _get_beam_process_at(self, cycle_time: float) -> lsa_settings.BeamProcess:
-        # If there are multiple particle transfers, find the first one
-        # that returns a beam process at the given time. Since we are
-        # dealing with functions, the beam processes of different
-        # particle transfers cannot overlap.
-        for transfer in self._parameter.getParticleTransfers():
-            beam_process = lsa_settings.Contexts.getFunctionBeamProcessAt(
-                self._cycle, transfer, cycle_time
+
+class IncorporatorGroup:
+    """Class that allows changing one function-type parameter.
+
+    This is the object-oriented alternative to the module-scope
+    functions. It is slightly more reusable in that it allows caching
+    the lookup of parameter and cycle.
+
+    Args:
+        parameter: The name of the parameter into which changes are to
+            be incorporated.
+        context: If passed, the context in which the parameter is
+            modified. In cycling machines like the SPS, this is the
+            cycle; in cycle-less machines like the LHC, this is the beam
+            process. Must not be passed together with *user*.
+        user: If passed, the user for which the parameter is modified.
+            Must not be passed together with *context*.
+    """
+
+    def __init__(
+        self,
+        parameters: t.List[str],
+        *,
+        context: t.Optional[str] = None,
+        user: t.Optional[str] = None,
+    ):
+        found_parameters = []
+        for name in parameters:
+            parameter = _services.parameter.findParameterByName(name)
+            if not parameter:
+                raise NotFound(name)
+            found_parameters.append(parameter)
+        self._parameters = tuple(found_parameters)
+        self._cycle = _find_cycle(context=context, user=user)
+
+    @property
+    def parameters(self) -> t.Tuple[str, ...]:
+        """The name of the current context."""
+        return tuple(p.getName() for p in self._parameters)
+
+    @property
+    def context(self) -> str:
+        """The name of the current context."""
+        return self._cycle.getName()
+
+    @context.setter
+    def context(self, name: str) -> None:
+        cycle = _services.context.findStandAloneCycle(name)
+        if not cycle:
+            raise NotFound(name)
+        self._cycle = cycle
+
+    @property
+    def user(self) -> t.Optional[str]:
+        """The name of the current user, or None if the context is unmapped."""
+        return self._cycle.getUser()
+
+    @user.setter
+    def user(self, name: str) -> None:
+        cycle = _services.context.findStandAloneContextByUser(name)
+        if not cycle:
+            raise NotFound(name)
+        assert isinstance(cycle, lsa_settings.StandAloneCycle), cycle
+        self._cycle = cycle
+
+    def incorporators(self) -> t.Iterator[Incorporator]:
+        """Iterate over incorporators for each of the group's parameters."""
+        for parameter in self._parameters:
+            # pylint: disable = protected-access
+            yield Incorporator._from_raw(parameter, self._cycle)
+
+    def get(self, parameter: str) -> Incorporator:
+        """Get an incorporator for a parameter in this group.
+
+        Raises:
+            KeyError if no parameter in the group has the given name.
+        """
+        for param in self._parameters:
+            if param.getName() == parameter:
+                # pylint: disable = protected-access
+                return Incorporator._from_raw(param, self._cycle)
+        raise KeyError(parameter)
+
+    def incorporate_and_trim(
+        self,
+        cycle_time: float,
+        values: t.Union[float, np.ndarray, t.Sequence[float], t.Mapping[str, float]],
+        *,
+        relative: bool,
+        description: t.Optional[str] = None,
+    ) -> None:
+        """Modify each function at a point and commit the change.
+
+        This assumes that each parameter is a scalar function. It
+        modifies its *correction* at a given point in time and submits
+        this modification to the LSA database. The *target* remains
+        unmodified.
+
+        Args:
+            cycle_time: The point at which each function shall be
+                modified. This is measured in milliseconds from the
+                beginning of the cycle. Only those points may be chosen
+                for which there is an incorporation rule in the LSA
+                database.
+            values: The correction values to be transmitted. How this
+                value is incorporated depends on the  keyword-only
+                argument *relative*. This may be a single float (to
+                incorporate the same correction to all parameters), a
+                sequence of floats (one for each parameter), or a
+                mapping from parameter name to float (which must mention
+                each parameter once and have no superfluous items).
+            relative: If True, the *values* are added to the correction
+                of the respective functions at the given time. If False,
+                the corrections are set to th *values*, overwriting the
+                previous correction at the given time.
+            description: The description to appear in LSA's trim
+                history. If not passed, an implementation-defined string
+                will be used.
+        """
+        values = self._canonicalize_values(values)
+        request_builder = (
+            lsa_settings.IncorporationRequest.builder()
+            .setContext(self._cycle)
+            .setDescription(
+                description if description is not None else DEFAULT_TRIM_DESCRIPTION
             )
-            if beam_process is not None:
-                return beam_process
-        raise NotFound(f"beam process for t_cycle={cycle_time} ms")
+            .setRelative(relative)
+        )
+        for parameter, value in zip(self._parameters, values):
+            if isinstance(value, np.floating):
+                value = float(value)
+            setting = _build_incorporation_setting(
+                parameter=parameter,
+                context=self._cycle,
+                cycle_time=cycle_time,
+                value=value,
+            )
+            request_builder.addIncorporationSetting(setting)
+        _services.trim.incorporate(request_builder.build())
+
+    def _canonicalize_values(
+        self,
+        values: t.Union[float, np.ndarray, t.Sequence[float], t.Mapping[str, float]],
+    ) -> np.ndarray:
+        if isinstance(values, t.Mapping):
+            return _canonicalize_dict(values, self._parameters)
+        return np.broadcast_to(values, (len(self._parameters),))
+
+
+def _canonicalize_dict(
+    values: t.Mapping[str, float], parameters: t.Iterable[lsa_settings.Parameter]
+) -> np.ndarray:
+    # Shallow-copy the dict, remove items from it. If anything is left,
+    # the user specified parameters that we don't know.
+    values = dict(values)
+    result = np.array([values.pop(p.getName()) for p in parameters])
+    if values:
+        raise KeyError(f"superfluous key: {values.popitem()[0]}")
+    return result
+
+
+def _find_cycle(
+    *, context: t.Optional[str], user: t.Optional[str]
+) -> lsa_settings.StandAloneCycle:
+    if context and user:
+        raise TypeError("conflicting arguments: `context` and `user`")
+    if user:
+        cycle = _services.context.findStandAloneContextByUser(user)
+        if not cycle:
+            raise NotFound(user)
+        assert isinstance(cycle, lsa_settings.StandAloneCycle), cycle
+        return cycle
+    if context:
+        cycle = _services.context.findStandAloneCycle(context)
+        if not cycle:
+            raise NotFound(context)
+        return cycle
+    raise TypeError("missing keyword-only argument: 'context'")
+
+
+def _build_incorporation_setting(
+    parameter: lsa_settings.Parameter,
+    context: lsa_settings.StandAloneContext,
+    cycle_time: float,
+    value: float,
+) -> lsa_settings.IncorporationSetting:
+    beam_process = _get_beam_process_at(parameter, context, cycle_time)
+    beam_process_time = cycle_time - beam_process.getStartTime()
+    setting = lsa_spi.ScalarSetting(acc_value.Type.DOUBLE)
+    setting.setBeamProcess(beam_process)
+    setting.setParameter(parameter)
+    setting.setCorrectionValue(acc_value.ValueFactory.createScalar(value))
+    return lsa_settings.IncorporationSetting(setting, beam_process_time)
+
+
+def _get_beam_process_at(
+    parameter: lsa_settings.Parameter,
+    context: lsa_settings.StandAloneContext,
+    cycle_time: float,
+) -> lsa_settings.BeamProcess:
+    # If there are multiple particle transfers, find the first one
+    # that returns a beam process at the given time. Since we are
+    # dealing with functions, the beam processes of different
+    # particle transfers cannot overlap.
+    for transfer in parameter.getParticleTransfers():
+        beam_process = lsa_settings.Contexts.getFunctionBeamProcessAt(
+            context, transfer, cycle_time
+        )
+        if beam_process is not None:
+            return beam_process
+    raise NotFound(f"beam process for {parameter.getName()} at {cycle_time} ms")
